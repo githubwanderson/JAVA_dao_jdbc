4eafa64b61f18039b092cbc65f2e2e3090d5863c
Wanderson <emailwandersonalves@gmail.com>
2022-11-28 13:12:44 -0300

    Add Department class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,14 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {	
+		
+		Department obj = new Department(1,"Books");
+		
+		System.out.println(obj);
 
 	}
 
