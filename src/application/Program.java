d8be19c8d115e61a1e19deb40d8bc312b4e142a7
Wanderson <emailwandersonalves@gmail.com>
2022-11-29 22:10:02 -0300

    add DepartmentDaoJDBC class
    starting DepartmentDaoJDBC::insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 import model.dao.DaoFactory;
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,24 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
+
+/**
+ * 
+ * Classe responsável por preservar os objetos de conexão das entidades
+ * @author WANDERSON
+ * 
+ */
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
